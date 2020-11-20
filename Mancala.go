b15a2a65799f1fced9d854196af525e715e1033f
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2020-11-20 09:38:31 -0500

    Revert back to let, broke something
    I didn't know what let did, but it broke something.

--- Mancala.go
+++ Mancala.go
@@ -49,9 +49,9 @@ func (Current *Board) Move(Position int) {
         return
     }
 
-    let ModifySide *[6]int
-    let Opposite *[6]int
-    let Points *int
+    var ModifySide *[6]int
+    var Opposite *[6]int
+    var Points *int
 
     if Current.Side {
         ModifySide = &Current.Right.Pieces
